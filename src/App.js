75d1bb35ab388cfb2244ec5cd4f1645f031e4087
irwinben <irwinben@yahoo.com>
2025-05-21 21:34:01 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>Welcome to the Holiday Meal Scheduler</h1>
+      <p>This is a starting point. You can build your app from here!</p>
+    </div>
+  );
+}
